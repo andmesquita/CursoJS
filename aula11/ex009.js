edbc84a6dd96248af287488050c6bc39bb4b269b
andmesquita <aandersonmesquita@gmail.com>
2022-04-05 11:08:41 -0300

    up

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,7 @@
 var país = 'EUA'
 console.log(`Vivendo em ${país}`)
 if(país == 'Brasil'){
-    console.log('Você éBrasileiro!')
+    console.log('Você é Brasileiro!')
 }else{
     console.log('Você é Estrangeiro')
 }
